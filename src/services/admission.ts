bbd4dc06a6a1008b35f9edf74ce5e0506e0802fb
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-09-11 13:29:58 -0500

    #13 - add e2e test suite (#14)
    * #13 - add e2e test suite
    
    * collect deployment logs on failure
    
    * add more diag collection
    
    * more diag collection
    
    * fix configmap
    
    * increase test timeout

--- src/services/admission.ts
+++ src/services/admission.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'inversify'
 import { TYPES } from '../types'
+import { Logger } from 'pino'
 
 export interface IAdmission {
   allowAdmission(containerImages: string[]): Promise<boolean>
@@ -10,14 +11,18 @@ export class Admission implements IAdmission {
   private readonly allowedImages: Set<string>
   private readonly blockedImages: Set<string>
   private readonly strictMode: boolean
+  private readonly logger: Logger
 
   constructor (
+    @inject(TYPES.Services.Logging)parentLogger: Logger,
     @inject(TYPES.Config.AllowedList)allowedImages: string[],
     @inject(TYPES.Config.BlockedList)blockedImages: string[],
     @inject(TYPES.Config.StrictMode)strictMode: boolean) {
+    this.logger = parentLogger.child({ module: 'services/Admission' })
     this.allowedImages = new Set(allowedImages.map((s) => s.toLowerCase()))
     this.blockedImages = new Set(blockedImages.map((s) => s.toLowerCase()))
     this.strictMode = strictMode
+    this.logger.info('loaded with {strictMode=%s, allowedImageCount=%d, blockedImageCount=%d, allowedImages=%j, blockImages=%j}', this.strictMode, this.allowedImages.size, this.blockedImages.size, Array.from(this.allowedImages), Array.from(this.blockedImages))
   }
 
   async allowAdmission (containerImages: string[]): Promise<boolean> {
--- test/services/admission.spec.ts
+++ test/services/admission.spec.ts
@@ -1,20 +1,24 @@
 import { describe, it, expect } from '@jest/globals'
 import 'reflect-metadata'
 import { Admission } from '../../src/services/admission'
+import pino from 'pino'
 
 describe('services/admission', () => {
+  const pinoLogger = pino({
+    level: 'error'
+  })
   it('Should block images on disallow list', async () => {
-    const service = new Admission(['allowed'], ['blocked'], false)
+    const service = new Admission(pinoLogger, ['allowed'], ['blocked'], false)
     const allow = await service.allowAdmission(['blocked'])
     expect(allow).toBeFalsy()
   })
   it('Should allow images not on disallow list', async () => {
-    const service = new Admission(['allowed'], ['blocked'], false)
+    const service = new Admission(pinoLogger, ['allowed'], ['blocked'], false)
     const allow = await service.allowAdmission(['allowed'])
     expect(allow).toBeTruthy()
   })
   it('Should only allow images on allow list when in restrictive mode', async () => {
-    const service = new Admission(['allowed'], ['blocked'], true)
+    const service = new Admission(pinoLogger, ['allowed'], ['blocked'], true)
     const allow = await service.allowAdmission(['no-on-list'])
     expect(allow).toBeFalsy()
   })
